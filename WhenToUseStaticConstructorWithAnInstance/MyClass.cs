7a4f7faa46c434cfa221c456dc1c2cda5a7217d9
Kulpot <kulpotwahu@gmail.com>
2023-06-01 06:09:17 +0800

    Add project files.

---
+++ WhenToUseStaticConstructorWithAnInstance/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ref link:https://www.youtube.com/watch?v=5B9oB2vMmvI&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=70
+
+namespace WhenToUseStaticConstructorWithAnInstance
+{
+    class MyClass
+    {
+        private static string[] _content;
+
+        static MyClass()
+        {
+            _content = File.ReadAllLines("t.txt");
+        }
+
+        public MyClass(int arraySize)
+        {
+            string[] temp = new string[arraySize];
+            Array.Copy(_content, temp, arraySize);
+            _content = temp;
+        }
+    }
+}
